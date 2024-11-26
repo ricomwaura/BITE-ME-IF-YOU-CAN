d7a64f53daac69ca74c65cb820e24bade0a22e63
Eric Mwaura <rico_mwaura@outlook.com>
2024-11-25 23:00:58 -0800

    feat(page): add RecipePage and styling to display chosen recipe.

---
+++ src/pages/RecipePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'styles/pages/RecipePage.css'
+
+const RecipePage: React.FC = () => {
+    return (
+        <div className={'recipe-page'}>
+
+        </div>
+    )
+}
+
+export default RecipePage;
