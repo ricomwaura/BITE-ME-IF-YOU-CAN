4a021501a0d8f53a8a04da04e203d86e628bdf6f
Eric Mwaura <rico_mwaura@outlook.com>
2024-11-25 22:59:56 -0800

    chore: update App component to include routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import RecipePage from 'pages/RecipePage';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
+/**
+ * App component defines the main application structure with routing.
+ * 
+ */
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='init'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<RecipePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
