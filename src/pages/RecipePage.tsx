31e5001884fc5955863a689035259106ebaa9231
Eric Mwaura <rico_mwaura@outlook.com>
2024-12-02 05:19:37 -0800

    feat(page): add RecipePage to project with styling.

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -1,10 +1,40 @@
+import RecipePageHeader from 'components/layout/RecipePageHeader';
+import { pancakeRecipe } from 'constants/PancakeRecipe'
+import { Recipe } from 'interface/Recipe'
 import React from 'react'
 import 'styles/pages/RecipePage.css'
 
+// Default pancake recipe.
+const selectedRecipe: Recipe = pancakeRecipe;
+
+/**
+ * RecipePAge component renders the main recipe page displaying the header, body and footer.
+ * 
+ * @component
+ * @example
+ * Example usage:
+ * 
+ * ```tsx
+ * import RecipePage from 'pages/RecipePage';
+ * 
+ * const App: React.FC = () => (
+ *      <div>
+ *          <RecipePage />
+ *      </div>
+ * );
+ * 
+ * export default App;
+ * ```
+ */
 const RecipePage: React.FC = () => {
     return (
         <div className={'recipe-page'}>
-
+            <RecipePageHeader 
+                id={selectedRecipe.id} 
+                title={selectedRecipe.title}
+                views={selectedRecipe.views} 
+                cover={selectedRecipe.imageMeta.cover}
+            />
         </div>
     )
 }
